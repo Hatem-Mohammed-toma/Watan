68604a894b192fe7567226f3cdb5df16d4445fd2
hatem mohammed <hatemze233@gmail.com>
2024-12-16 00:23:49 +0200

    the last last

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
+    ],build: {
+        outDir: 'dist', // Ensure this is set to the desired directory
+    },
 });
+
